863110b0805d23ae005438b4d7d24620f8837907
adworjan <adworjan@redhat.com>
2025-05-29 17:34:01 -0400

    remove lock file from gitignore and added required_providers

--- roles/shadowman_terraform_azure/templates/EXAMPLEazuremain2vm.tf
+++ roles/shadowman_terraform_azure/templates/EXAMPLEazuremain2vm.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.29"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
 provider "azurerm" {
   features {}
 }
--- roles/shadowman_terraform_vsphere/files/EXAMPLEvmwaremain3vms.tf
+++ roles/shadowman_terraform_vsphere/files/EXAMPLEvmwaremain3vms.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source  = "hashicorp/vsphere"
+      version = "~> 2.10"
+    }
+  }
+
+  required_version = "~> 1.2"
+}
+
 provider "vsphere" {
   # If you have a self-signed cert
   allow_unverified_ssl = true
